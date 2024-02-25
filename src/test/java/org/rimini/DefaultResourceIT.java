9bb887e7c36b63222048ed9feb1ce44ccda07464
Szymon Wilk <szymon.wilk.72@gmail.com>
2024-02-25 18:52:59 +0100

    Initial commit

---
+++ src/test/java/org/rimini/DefaultResourceIT.java
@@ -0,0 +1,8 @@
+package org.rimini;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class DefaultResourceIT extends DefaultResourceTest {
+    // Execute the same tests but in packaged mode.
+}
